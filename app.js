9f8331cd2774b4a7eaa8ce97a2a9ebdd3d5014bc
Hany Soliman <Hany.essam1993@gmail.com>
2020-01-24 19:36:23 +0200

    Authentication Level 2 (Mongoose-Encryption)

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 mongoose.connect('mongodb://localhost:27017/userDB', {
@@ -18,32 +22,39 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
   useUnifiedTopology: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret";
+
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedField: ["Password"]
+});
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res){
+app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
     password: req.body.password
   });
 
-  newUser.save(function(err){
+  newUser.save(function(err) {
     if (err) {
       console.log(err);
     } else {
@@ -53,11 +64,13 @@ app.post("/register", function(req, res){
 });
 
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
@@ -69,6 +82,6 @@ app.post("/login", function(req, res){
     }
   });
 });
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("server started on port 3000");
 });
