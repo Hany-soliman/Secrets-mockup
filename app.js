8f583bc33bd502ea0b730d96187e1a6ef39b7a6c
Hany Soliman <Hany.essam1993@gmail.com>
2020-01-25 04:07:22 +0200

    Authentication Level 4 (Hashed password with saltRounds *bcrypt*)

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({
     email: username
